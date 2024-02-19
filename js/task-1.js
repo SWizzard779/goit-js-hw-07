8767ca585919ce997794a7e2eacb1da0fdf0d65b
SWizzard779 <sofia.nesimko@gmail.com>
2024-02-19 18:12:46 +0100

    task_1

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,11 @@
-function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
-}
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+const listAmount = document.querySelectorAll('.item');
+console.log(`Number of categories: ${listAmount.length}`);
+
+const h2Elements = document.querySelectorAll('h2');
+
+h2Elements.forEach(h2 => {
+  const ul = h2.nextElementSibling;
+  const lis = ul.querySelectorAll('li');
+  console.log(`Category: ${h2.textContent}`);
+  console.log(`Elements: ${lis.length}`);
+});
