a81f2d5d9c78eb231433b498a013ee27c47f058e
SWizzard779 <sofia.nesimko@gmail.com>
2024-02-23 19:55:51 +0100

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('.change-color');
+btn.addEventListener('click', handleBtn);
+
+function handleBtn(event) {
+  console.log(event);
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+btn.style.borderRadius = '8px';
+btn.style.backgroundColor = '#4e75ff';
+btn.style.padding = '8px 16px';
+btn.style.width = '148px';
+btn.style.height = '40px';
+btn.style.color = '#fff';
+btn.style.borderStyle = 'none';
